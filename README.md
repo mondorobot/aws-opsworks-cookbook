0e63927d9d81884fa9851c127e23cb1918ca93e0
cprater <cprater23@gmail.com>
2018-09-12 09:41:24 -0600

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# aws-opsworks-cookbook
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: aws-opsworks-cookbook
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
